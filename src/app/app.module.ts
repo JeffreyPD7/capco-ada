36344d2b4f3a2ea4c3a2288c51cd6361daa98432
Jeffrey Paulino <jp7kode@gmail.com>
2018-10-04 10:34:17 -0400

    Partially set up routes, home template and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { DataTableModule } from './data-table/data-table.module';
@@ -12,6 +15,12 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '**', redirectTo: 'home', pathMatch: 'full' }
+    ]),
     DataTableModule
   ],
   providers: [],
