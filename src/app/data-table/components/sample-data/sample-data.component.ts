9b0557d13f0f18696e726d2131740abf898f9950
Jeff <jp7kode@gmail.com>
2018-10-06 22:43:07 -0400

    Pagination working, finishing a few things

--- src/app/data-table/components/sample-data/sample-data.component.ts
+++ src/app/data-table/components/sample-data/sample-data.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 
-import { SampleDataService } from '../../services/sample-data.service';
-import { ISampleData } from '../../interfaces/sample-data.interface';
-import { NgForm } from '@angular/forms';
+import {SampleDataService} from '../../services/sample-data.service';
+import {ISampleData} from '../../interfaces/sample-data.interface';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-sample-data',
@@ -23,7 +23,26 @@ export class SampleDataComponent implements OnInit {
 
   JSON: JSON;
 
-  constructor(private _sampleData: SampleDataService) { }
+
+  // Pagination Section ---------------------------------------
+  // public numOne: number;
+  // public numTwo: number;
+  public currentPage: number = 1;
+  // public numFour: number;
+  // public numFive: number;
+  public newDataList: ISampleData[] = [];
+
+  public sliceStart: number = 0;
+  public sliceEnd: number = 25;
+  public numOfPages: number = 8;
+
+
+
+  // end of pagination ----------------------------------------
+
+
+  constructor(private _sampleData: SampleDataService) {
+  }
 
   ngOnInit() {
     this._sampleData.getData()
@@ -41,6 +60,33 @@ export class SampleDataComponent implements OnInit {
         {id: i, value: 25 * i}
       );
     }
+
+    // alert(this.dataList[].length);
+    // alert(Object.keys(this.dataList).length);
   }
+
+  // Pagination Section ---------------------------------------
+  public nextPage(): void {
+    this.sliceStart = this.sliceEnd;
+    this.sliceEnd = this.sliceStart + this.selectedFilter;
+    this.currentPage++;
+  }
+
+  public previousPage(): void {
+    this.sliceEnd = this.sliceStart;
+    this.sliceStart = this.sliceStart - this.selectedFilter;
+    this.currentPage--;
+  }
+
+
+  public filterChange(event): void {
+    this.sliceStart = 0;
+    this.sliceEnd = event;
+    this.currentPage = 1;
+    this.numOfPages = 200 / this.selectedFilter;
+    // alert(`Value is ${event}`);
+  }
+
+  // end of pagination ----------------------------------------
 
 }
