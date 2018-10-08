7322c16c80629fbe057bf0ce60d0dbfde598bffe
Jeff <JP7Kode@gmail.com>
2018-10-08 15:13:59 -0400

    Partially set POST request, still workin on it and organizing code

--- src/app/shared/components/table-shared/table-shared.component.ts
+++ src/app/shared/components/table-shared/table-shared.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 
 import {SampleDataService} from '../../services/sample-data.service';
 import {ISampleData} from '../../interfaces/sample-data.interface';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {catchError} from 'rxjs/operators';
 
 @Component({
   selector: 'app-table-shared',
@@ -16,7 +18,10 @@ export class TableSharedComponent implements OnInit {
   public dataList: ISampleData[] = [];
   public errorMsg: string;
 
-  constructor(private _sampleData: SampleDataService) { }
+  constructor(
+    private _sampleData: SampleDataService,
+    private httpClient: HttpClient
+  ) { }
 
   ngOnInit() {
     this._sampleData.getData()
@@ -24,5 +29,22 @@ export class TableSharedComponent implements OnInit {
         data => this.dataList = data,
         error => this.errorMsg = <any>error
       );
+  }
+
+  public saveInfo(rowId: number, rowStatus: string): void {
+    const infoObject: object = {
+      id: rowId,
+      status: rowStatus
+    };
+
+    this.httpClient.post<any>('http://localhost:3000/sample_submit',
+      infoObject, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      });
+
+    alert(`POST Object ${JSON.stringify(infoObject)}`);
+    // alert(`This is the rowID ${rowId} and rowStatus ${rowStatus}`);
   }
 }
