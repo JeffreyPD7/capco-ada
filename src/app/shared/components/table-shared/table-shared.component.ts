3547ce644e45c08878e1c3641c8a8b807e1fa44f
Jeff <jp7kode@gmail.com>
2018-10-08 21:04:29 -0400

    Installing json-server in order run submit.json file for submission

--- src/app/shared/components/table-shared/table-shared.component.ts
+++ src/app/shared/components/table-shared/table-shared.component.ts
@@ -2,8 +2,11 @@ import {Component, Input, OnInit} from '@angular/core';
 
 import {SampleDataService} from '../../services/sample-data.service';
 import {ISampleData} from '../../interfaces/sample-data.interface';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import {catchError} from 'rxjs/operators';
+
+import {throwError} from 'rxjs';
+import {IDataPost} from '../../interfaces/data-post';
 
 @Component({
   selector: 'app-table-shared',
@@ -32,19 +35,30 @@ export class TableSharedComponent implements OnInit {
   }
 
   public saveInfo(rowId: number, rowStatus: string): void {
-    const infoObject: object = {
-      id: rowId,
-      status: rowStatus
-    };
+    this.httpClient.post<IDataPost>('http://localhost:3000/submitInfo',
+      {
+        'id': rowId,
+        'status': rowStatus
+      })
+      .subscribe(
+        data => {
+          console.log('POST Request is successful ', data);
+        },
+        error => {
+          console.log('Error', error);
+        }
+      );
 
-    this.httpClient.post<any>('http://localhost:3000/sample_submit',
-      infoObject, {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json'
-        })
-      });
-
-    alert(`POST Object ${JSON.stringify(infoObject)}`);
-    // alert(`This is the rowID ${rowId} and rowStatus ${rowStatus}`);
+    alert(`Row of ID ${rowId} and Status of ${rowStatus} has been submitted to api/submit`);
   }
+
+  // private handleError(errorResponse: HttpErrorResponse) {
+  //   if (errorResponse.error instanceof ErrorEvent) {
+  //     console.error('Client Side Error :', errorResponse.error.message);
+  //   } else {
+  //     console.error('Server Side Error :', errorResponse);
+  //   }
+  //
+  //   return throwError('There is a problem with the service.We are notified & working on it. Please try again later.');
+  // }
 }
---
+++ src/app/shared/interfaces/data-post.ts
@@ -0,0 +1,5 @@
+
+export interface IDataPost {
+  id: number;
+  status: string;
+}
